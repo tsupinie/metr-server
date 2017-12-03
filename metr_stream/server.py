52ba2ad17d654d3079616e77a2e68e02ffb8ccdf
tsupinie <tsupinie@gmail.com>
2017-12-02 23:10:55 -0600

    Added disk cleaner process to delete old data

--- metr_stream/server.py
+++ metr_stream/server.py
@@ -2,6 +2,8 @@
 import logging
 import asyncio
 import signal
+import os
+from datetime import datetime, timedelta
 
 from autobahn.asyncio.websocket import WebSocketServerFactory
 
@@ -9,11 +11,53 @@ from protocols.hollaback import HollaBackProtocol
 from protocols.metr_stream import MetrStreamProtocol
 
 
+class Cleaner(object):
+    def __init__(self, interval, max_age, data_dir):
+        self._intv = interval
+        self._max_age = timedelta(seconds=max_age)
+        self._path = data_dir
+
+        self._logger = logging.getLogger(__name__)
+        self._logger.setLevel(logging.INFO)
+
+    async def run_cleaner(self):
+        self._logger.info("Starting cleaner process")
+        while True:
+            await asyncio.sleep(self._intv)
+
+            try:
+                self._cleanup()
+            except Exception as exc:
+                self._logger.error(str(exc))
+
+    def _cleanup(self):
+        now = datetime.utcnow()
+        self._logger.info(f"Cleaning '{self._path}'")
+        for root, dnames, fnames in os.walk(self._path):
+            if os.path.basename(root) in ['geo', 'l2raw']:
+                continue
+
+            self._logger.debug(f"Cleaning '{root}'")
+
+            for fname in fnames:
+                if not fname.endswith('.json'):
+                    continue
+
+                full_fname = os.path.join(root, fname)
+
+                dt = datetime.utcfromtimestamp(os.path.getmtime(full_fname))
+                if dt < now - self._max_age:
+                    os.unlink(full_fname)
+
+
 def main():
     port = 8001
+    data_path = "data"
     logging.basicConfig(format="%(levelname)s|%(name)s|%(asctime)-15s: %(message)s")
     logger = logging.getLogger(__name__)
     logger.setLevel(logging.INFO)
+
+    MetrStreamProtocol.data_path = data_path
 
     factory = WebSocketServerFactory()
     factory.protocol = MetrStreamProtocol
@@ -21,6 +65,9 @@ def main():
     loop = asyncio.get_event_loop()
     coro = loop.create_server(factory, '127.0.0.1', port)
     server = loop.run_until_complete(coro)
+
+    cleaner = Cleaner(300, 6 * 3600, data_path)
+    clnr = asyncio.ensure_future(cleaner.run_cleaner())
 
     def staaahp(sig_num, stack_frame):
         raise KeyboardInterrupt()
